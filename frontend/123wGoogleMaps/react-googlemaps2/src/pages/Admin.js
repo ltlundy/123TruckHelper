766933d34f6f8cb3f132e43b317ffebf0c78591b
Alex Lu <alexander.lu2@mail.mcgill.ca>
2023-11-18 05:35:30 -0500

    Updating gitignores

--- frontend/123wGoogleMaps/react-googlemaps2/src/pages/Admin.js
+++ frontend/123wGoogleMaps/react-googlemaps2/src/pages/Admin.js
@@ -1,8 +1,5 @@
 import React from "react";
-// import Container from "react-bootstrap/Container";
 import { Navibar } from "../components/Frame.js";
-
-import Gallery from "../components/Gallery.js";
 
 function Admin() {
   return (
