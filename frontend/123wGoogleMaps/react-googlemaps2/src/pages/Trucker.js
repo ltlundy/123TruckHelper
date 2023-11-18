65b37c34adf07aaefa43a43122fc1a2f37c4ae83
Alex Lu <alexander.lu2@mail.mcgill.ca>
2023-11-18 17:43:09 -0500

    Added map to trucker.js

--- frontend/123wGoogleMaps/react-googlemaps2/src/pages/Trucker.js
+++ frontend/123wGoogleMaps/react-googlemaps2/src/pages/Trucker.js
@@ -1,13 +1,73 @@
 import React from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { Navibar } from "../components/Frame.js";
 
-function Trucker() {
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "70vh",
+};
+const center = {
+  lat: 7.2905715, // default latitude
+  lng: 80.6337262, // default longitude
+};
+// console.log("Key is: " + process.env.REACT_APP_GOOGLE_MAPS_KEY)
+const Trucker = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+    libraries,
+  });
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
   return (
     <div>
       <Navibar />
-      <h1>Trucker Page</h1>
+      <div>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={10}
+          center={center}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </div>
+      <hr></hr>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
-}
+};
 
 export default Trucker;
