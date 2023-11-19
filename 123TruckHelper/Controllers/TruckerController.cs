dc81d3e2195202f180b2e95d3968d221dd32a001
ltlundy <liam.lund@mail.mcgill.ca>
2023-11-19 01:07:43 -0500

    Adding Twilio

---
+++ 123TruckHelper/Controllers/TruckerController.cs
@@ -0,0 +1,23 @@
+﻿using _123TruckHelper.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _123TruckHelper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TruckerController : ControllerBase
+    {
+        private ITruckService TruckService { get; }
+
+        public TruckerController(ITruckService TruckService)
+        {
+            this.TruckService = TruckService;
+        }
+
+        [HttpPost("/PhoneNumber/{phoneNumber}")]
+        public async Task AddPhoneNumber(string phoneNumber)
+        {
+
+        }
+    }
+}
