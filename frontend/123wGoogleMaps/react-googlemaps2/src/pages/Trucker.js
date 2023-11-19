7a7e871f69dc256a3b30c51f0cd2f6650d2f0fcf
Alex Lu <alexander.lu2@mail.mcgill.ca>
2023-11-19 00:12:39 -0500

    WOO mock api reconfigured Trucker fetching from mock data to make panels Use json-server --watch ./testing/mockdata2.json --port 8000

--- frontend/123wGoogleMaps/react-googlemaps2/src/pages/Trucker.js
+++ frontend/123wGoogleMaps/react-googlemaps2/src/pages/Trucker.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { Navibar } from "../components/Frame.js";
-import PolledComponent from "../components/PolledComponent.js";
+// import { MyComponent } from "../components/PolledComponent.js";
+import { usePageVisibility } from "../components/usePageVisibility.js";
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -10,8 +11,6 @@ import { useEffect, useState, useRef } from 'react';
 
 import logo from '../components/123loadboard.png';
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const libraries = ["places"];
@@ -31,17 +30,43 @@ const Trucker = () => {
   });
 
   const [posts2, set2Posts] = useState([]);
-   useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+  const isPageVisible = usePageVisibility();
+  const timerIdRef = useRef(null);
+
+  useEffect(() => {
+    const pollingCallback = () => {
+      // fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      fetch('http://localhost:8000/datum')
          .then((response) => response.json())
          .then((data) => {
-            console.log(data);
+            console.log(data); // DEBUGGING
             set2Posts(data);
          })
          .catch((err) => {
             console.log(err.message);
          });
-   }, []);
+         console.log('Polling...');
+    }
+    const startPolling = () => {
+      pollingCallback(); // To immediately start fetching data
+      // Polling every 30 seconds CHANGE REFRESH RATE
+      timerIdRef.current = setInterval(pollingCallback, 1000);
+    };
+
+    const stopPolling = () => {
+      clearInterval(timerIdRef.current);
+    };
+
+    if (isPageVisible) {
+      startPolling();
+    } else {
+      stopPolling();
+    }
+
+    return () => {
+      stopPolling();
+    };
+  }, [isPageVisible]);
 
   if (loadError) {
     return <div>Error loading maps</div>;
@@ -65,16 +90,16 @@ const Trucker = () => {
       </div>
       <hr></hr>
       <div id="testGround">
-        {/* <PolledComponent></PolledComponent> */}
+        {/* <MyComponent /> */}
         {/* <div className="posts-container" > */}
-        <div class="flex-container">
+        <div className="flex-container">
           {/* <Container>
             <Row> */}
               {posts2.map((post) => {
                 return (
-                    <div key={post.id} class="flex-item">
+                    <div key={post.id} className="flex-item">
                       <Col>
-                        <Card style={{ width: '18rem' }}>
+                        <Card style={{ width: '18rem', padding: '15px'}}>
                           <Card.Img variant="top" src={logo} />
                           <Card.Body>
                             <Card.Title>{post.title}</Card.Title>
