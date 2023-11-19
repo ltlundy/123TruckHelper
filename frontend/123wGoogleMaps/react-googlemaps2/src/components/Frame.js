5afc11e4d26a4a4f3c3fa2764ff439cb0e416ec3
Arman Shroff-Mehrabadi <arman.shroff-mehrabadi@mail.mcgill.ca>
2023-11-19 02:17:56 -0500

    Squashed commit of the following:
    commit 1550fcc5c84edd785b5f75e73f2f33d3406bc1fd
    Author: Alex Lu <alexander.lu2@mail.mcgill.ca>
    Date:   Sun Nov 19 02:11:01 2023 -0500
    
        Updating message cards in Trucker.js with revenue.
    
    commit 1272054afd0ed4913cae024458f208773a897f95
    Author: Alex Lu <alexander.lu2@mail.mcgill.ca>
    Date:   Sun Nov 19 01:21:02 2023 -0500
    
        Minor cleaning, commenting out unused variablesetc

--- frontend/123wGoogleMaps/react-googlemaps2/src/components/Frame.js
+++ frontend/123wGoogleMaps/react-googlemaps2/src/components/Frame.js
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 export function Navibar() {
   return (
